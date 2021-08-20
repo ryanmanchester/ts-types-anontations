52cf87b2fe7863ae5d283374495693042296b32c
Ryan Manchester <ryanm17@gmail.com>
2021-08-20 19:35:48 -0400

    exploring type anontations in TS

---
+++ annontations/variables.ts
@@ -0,0 +1,26 @@
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothingToSee: undefined = undefined;
+
+//built in objects
+let now: Date = new Date();
+
+//Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3, 5];
+let truths: boolean[] = [true, true, false, true];
+
+//Classes
+class Car {
+
+}
+let car: Car = new Car();
+
+//Object Literal
+let point: {x: number; y: number; } = {
+  x: 10,
+  y: 20
+};
