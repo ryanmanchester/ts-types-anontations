6504f7b68864015d856521f9019b991513f74aef
Ryan Manchester <ryanm17@gmail.com>
2021-08-20 20:17:58 -0400

    annotations with functions in TS

--- annontations/variables.ts
+++ annontations/variables.ts
@@ -34,3 +34,21 @@ const logNumber: (i: number) => void = (i: number) => {
 //1. Function that returns the 'any' type
 const json = '{"x": 10, "y": 20}';
 const coordinates: {x: number, y: number} = JSON.parse(json);
+
+//2. When we declare a variable and initialize later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+for(let i = 0; i < words.length; i++) {
+  if(words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+//3. Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+for(let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i]
+  }
+}
